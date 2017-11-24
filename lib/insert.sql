8f6763e69e33ef0988b36d95e44c7df5384e96f3
jacobgwiggins <jacobgwiggins9@gmail.com>
2017-11-24 10:20:50 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,3 +2,12 @@ INSERT INTO series (title, author_id, subgenre_id)
 VALUES
 ("Series One", 1, 1),
 ("Series Two", 2, 2);
+
+INSERT INTO books (title, year, series_id)
+VALUES
+("Book 1", 1981, 1),
+("Book 2", 1982, 2),
+("Book 3", 1983, 3),
+("Book 4", 1984, 4),
+("Book 5", 1985, 5),
+("Book 6", 1986, 6);
