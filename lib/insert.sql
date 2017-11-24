611b23040d0b728bca54d07e30be0c8147908ab0
jacobgwiggins <jacobgwiggins9@gmail.com>
2017-11-24 10:21:13 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -11,3 +11,14 @@ VALUES
 ("Book 4", 1984, 4),
 ("Book 5", 1985, 5),
 ("Book 6", 1986, 6);
+
+INSERT INTO characters (name, species, motto, series_id, author_id)
+VALUES
+("Char 1", "Human", "Motto 1", 1, 1),
+("Char 2", "Human", "Motto 2", 1, 1),
+("Char 3", "Human", "Motto 3", 1, 1),
+("Char 4", "Human", "Motto 4", 1, 1),
+("Char 5", "Human", "Motto 5", 1, 1),
+("Char 6", "Human", "Motto 6", 1, 1),
+("Char 7", "Human", "Motto 7", 1, 1),
+("Char 8", "Human", "Motto 8", 1, 1);
